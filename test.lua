bd3715ed4eb4b4074e168d5591f11166b597f10c
Jonathan Barton <tmtowtdi@gmail.com>
2016-10-30 02:56:41 -0400

    Adding reds bin and lib.  Getting vectors from GPS

--- test.lua
+++ test.lua
@@ -1,21 +1,8 @@
 #!/usr/bin/env lua
 
-local cooked = {
-  name = 'jon',
-  age = 47
-}
+local mybool = true
 
-
-function foo()
-  local names = {}
-  for k,v in pairs(cooked) do
-    table.insert(names,k)
-  end
-  return names
+for i=1,5 do
+  print(mybool)
+  mybool = not mybool
 end
-
-local n = foo()
-for i = 1,#n do print(n[i]) end
-
-
- -- vim: syntax=lua tw=39 ts=2 sw=2
